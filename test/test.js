d81a94cb8dcb37c90148545e23a65cd85836e490
neozhang <neozhang@cool33.com>
2019-09-16 14:00:19 +0800

    20190916

--- test/test.js
+++ test/test.js
@@ -1,11 +1,5 @@
 'use strict';
 
-var expect = require('expect');
-const task = require('../dist/task');
-
 describe('gulp task test', () => {
-	it('should return 2', () => {
-		var tsk = task.define('compile', () => {
-        });
-	});
+	it('should return 2', () => {});
 });
